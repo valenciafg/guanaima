4c4df08d2aa83d65a6e5ce8addaa554bf493af1d
Victor Valencia <valencia6x@gmail.com>
2016-08-19 21:52:58 -0400

    tsconfig update and layout directory created

--- assets/scripts/app/main.ts
+++ assets/scripts/app/main.ts
@@ -1,6 +1,3 @@
-/*import { bootstrap }    from '@angular/platform-browser-dynamic';
-import { AppComponent } from './app.component';
-bootstrap(AppComponent); */
 import {AppComponent} from './app.component';
 import {bootstrap} from '@angular/platform-browser-dynamic';
 import {HTTP_PROVIDERS} from '@angular/http';
